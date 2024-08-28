34016170d42e77f2f21c56b843732f744adc1ccc
shubham mishra <shubhamsm97@gmail.com>
2024-08-28 17:47:27 +0530

    assignment7.0

---
+++ assignment7.0/assignment7.3.js
@@ -0,0 +1,9 @@
+
+function inside(){
+const arr=document.querySelectorAll("a");
+ arr.forEach(element =>{
+    element.style.border='2px solid yellow';
+    element.style.padding="7px"
+    element.style.borderRadius='10px';
+});
+}
